f763656f7650c42c17edafeaee951e9be99b8dab
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-04 18:00:22 +0100

    aggiunta numeri, completo

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-// console.log("Il tuo cognome e':" + cognome_utente);
-//
-// console.log(lista_cognomi);
-//
-// console.log(lista_cognomi);
-
-
-// Chiedi all'utente il cognome (var in promp)
-// Inseriscilo in un array che ha altri cognomi dentro (creo array + push nome utente)
-// stampa la lista ordinata alfabeticamente (confronto con un ciclo while)
 
 var cognome_utente = prompt("Inserisci il tuo cognome");
-
 var cognome_utente_corretto = cognome_utente.charAt(0).toUpperCase() + cognome_utente.slice(1);
-
 var lista_cognomi = ["Rembrant", "Picasso", "Vinci"];
 
 console.log(lista_cognomi);
@@ -20,4 +8,10 @@ console.log(lista_cognomi);
 lista_cognomi.push(cognome_utente_corretto);
 
 lista_cognomi.sort();
-document.getElementById("lista").innerHTML = lista_cognomi;
+
+for (var i = 0; i < lista_cognomi.length; i++) {
+  lista_cognomi[i] = (i+1) + ") " + lista_cognomi[i] + "<br>";
+}
+var risultato = lista_cognomi.toString().replace(/,/g, " ");
+
+document.getElementById("lista").innerHTML = risultato;
